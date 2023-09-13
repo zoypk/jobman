3babb0bcd14d0981badbe4c511a28258809d2df2
Mohammed Zohil PK <zohilfazal@gmail.com>
2023-09-13 16:26:53 +0530

    v2

--- app.py
+++ app.py
@@ -3,14 +3,42 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from datetime import datetime 
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '998251e474ef6a9ae2b6b0804e7d4eb0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app )
+bcrypt = Bcrypt(app)
+loginmanager = LoginManager(app)
 
-from models import User, Post
+
+@loginmanager.user_loader
+def load_user(user_id):
+      return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+      id = db.Column(db.Integer, primary_key=True)
+      username = db.Column(db.String(20), unique=True, nullable=False)
+      email = db.Column(db.String(120), unique=True, nullable=False)
+      image_file = db.Column(db.String(20), default='default.img', nullable=False)
+      password = db.Column(db.String(60), nullable=False)
+      posts = db.relationship('Post', backref='author', lazy=True)
+
+      def __repr__(self):
+            return f"User ('{self.username}', '{self.email}', '{self.image_file}')"
+      
+class Post(db.Model):
+      id = db.Column(db.Integer, primary_key=True)
+      title = db.Column(db.String(100), nullable=False)
+      location = db.Column(db.String(100), nullable=False)
+      content = db.Column(db.Text, nullable=False)
+      level = db.Column(db.String(100), nullable=False)
+      user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+      
+      def __repr__(self):
+           return f"Post('{self.title}')"      
+
 
 posts = [
     {
@@ -41,6 +69,10 @@ def about():
 def signup():
         form = RegistrationForm()
         if form.validate_on_submit():
+              hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+              user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+              db.session.add(user)
+              db.session.commit()
               flash(f'Account created for {form.username.data}!', 'success')
               return redirect(url_for('login'))
         return render_template('signup.html',form = form)
@@ -48,12 +80,23 @@ def signup():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
         form = LoginForm()
-            
+        if form.validate_on_submit():
+              user = User.query.filter_by(email=form.email.data).first()
+              if user and bycrypt.check_password_hash(user.password, form.password.data):
+                    login_user(user, remember=form.remember.data)
+                    return redirect(url_for('home'))
+              else:
+                    flash('Login unsuccessfull. Please enter correct credentials')
         return render_template('login.html', form = form)
 
+@app.route("/logout")
+def logout():
+      logout_user()
+      return redirect(url_for('home'))      
 
-
-
+@app.route("/account")
+def account():
+        return render_template('account.html', form = form)
 
 
 
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [InputRequired(), Length(min=2, max=20)])
@@ -8,6 +8,14 @@ class RegistrationForm(FlaskForm):
     password =  PasswordField('Password', validators = [InputRequired(),  ])
     confirmPassword = PasswordField('Password', validators = [InputRequired(), EqualTo('password') ])
     submit = SubmitField('Sign Up')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first() 
+        if user:
+            raise ValidationError('Username not available')
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first() 
+        if email:
+            raise ValidationError('email not available')
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators = [InputRequired(), Email() ])
